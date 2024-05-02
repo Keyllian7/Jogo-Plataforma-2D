4748468e0e94d907d3d62cb0a8ff4a9965144a1b
Tiago Ferreira <tiagosilvaff18@gmail.com>
2024-05-01 22:09:41 -0300

    Login-OK

--- tela_login.py
+++ tela_login.py
@@ -11,9 +11,7 @@ pygame.init()
 
 # Defina as cores
 BRANCO = (255, 255, 255)
-PRETO = (0, 0, 0)
-AZUL = (0, 0, 255)
-
+#Diretorios de arquivos para o codigo
 diretorio_principal = os.path.dirname(__file__)
 diretorio_imagens = os.path.join(diretorio_principal, 'Assets Imagens')
 diretorio_sons = os.path.join(diretorio_principal, 'Assets Sons')
@@ -32,15 +30,16 @@ Usuario_Login = pygame.transform.scale(Usuario_Login, (150, 46.4))
 #Imagem do botão "Iniciar Jogo"
 iniciar_Jogo = pygame.image.load(os.path.join(diretorio_imagens,'Iniciar Jogo UNP.png'))
 iniciar_Jogo = pygame.transform.scale(iniciar_Jogo, (150, 56.4))
+#Criação do botão iniciar
 iniciar_Jogo_rect = iniciar_Jogo.get_rect()
 iniciar_Jogo_rect.center = (483,372)
 #Background da tela 
 imagem_de_fundo = pygame.image.load(os.path.join(diretorio_imagens, 'Background.png'))
 imagem_de_fundo = pygame.transform.scale(imagem_de_fundo, (largura, altura))
-
+#Sprite Personagem do jogo
 spritesheet_andar_direita = pygame.image.load(os.path.join(diretorio_imagens, 'PassosDireita.png')).convert_alpha()
 spritesheet_andar_esquerda = pygame.image.load(os.path.join(diretorio_imagens, 'PassosEsquerda.png')).convert_alpha()
-
+#Personagem do jogo
 personagem = Masculino(spritesheet_andar_direita, spritesheet_andar_esquerda)
 sprites_personagem = pygame.sprite.Group()
 sprites_personagem.add(personagem)
@@ -48,7 +47,7 @@ sprites_personagem.add(personagem)
 # Defina a fonte para o texto
 fonte_usuario = pygame.font.Font(None, 25)
 fonte = pygame.font.Font(None, 30)
-
+#Função do click
 clicked = False
 
 # Variável para armazenar o nome do usuário
@@ -75,19 +74,19 @@ while rodando:
                 # Adicionar caracteres digitados ao nome do usuário
                 nome_usuario += evento.unicode
 
+        #Função do Iniciar jogo
         elif evento.type == pygame.MOUSEBUTTONDOWN:
             if iniciar_Jogo_rect.collidepoint(evento.pos):
                 clicked = True
 
-    # Preencher a tela com a cor de fundo
+    #Movimentar o fundo
     tela.blit(imagem_de_fundo, (0,0))
     mov_tela = altura % imagem_de_fundo.get_rect().width
     tela.blit(imagem_de_fundo, (mov_tela - imagem_de_fundo.get_rect().width, 0))
     if mov_tela < 960:
         tela.blit(imagem_de_fundo, (mov_tela,0))
     altura-= 1/2
-
-
+    # Desenhar o título do jogo
     tela.blit(unp_survival, (230,0))
     # Desenhar o campo de entrada de texto
     texto_nome = fonte_usuario.render(nome_usuario, True, BRANCO)
@@ -98,17 +97,16 @@ while rodando:
 
     # Desenhar o botão de iniciar o jogo
     tela.blit(iniciar_Jogo, iniciar_Jogo_rect,)
-
+    # Desenhar o personagem
     sprites_personagem.draw(tela, (500, 460))
 
 
 
     # Atualizar a tela
     pygame.display.flip()
-
+    #Abrir o Jogo ao clicar no "Iniciar Jogo"
     if clicked:
-        subprocess.Popen(["python", "Jogo-Plataforma-2D\main.py"])
-        print('Clic')
+        subprocess.Popen(["python", "Jogo-Plataforma-2D\main.py"]) 
         pygame.quit()
         clicked = False
 
