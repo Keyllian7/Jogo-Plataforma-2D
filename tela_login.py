ada32b8e351ef724ac0353c48f95c8db6a427ba9
Tiago Ferreira <tiagosilvaff18@gmail.com>
2024-05-01 21:46:05 -0300

    Tela Login-OK

--- tela_login.py
+++ tela_login.py
@@ -4,6 +4,7 @@ from sys import exit
 from player import Personagem, Masculino, Feminino
 from inimigos import Inimigo, Vampiro, Lobisomem, Zumbi
 from plataformas import Plataforma
+import subprocess
 
 # Inicialize o Pygame
 pygame.init()
@@ -22,22 +23,33 @@ largura = 960
 altura = 540
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption('Unp Survival - Login')
-
+#Titulo do jogo
 unp_survival = pygame.image.load(os.path.join(diretorio_imagens,'Titulo UNP.png')).convert_alpha()
 unp_survival = pygame.transform.scale(unp_survival, (500, 125.49))
-
+#Imagem do Nome "Usuario"
 Usuario_Login = pygame.image.load(os.path.join(diretorio_imagens,'Usuário UNP.png'))
 Usuario_Login = pygame.transform.scale(Usuario_Login, (150, 46.4))
-
-inicar_Jogo = pygame.image.load(os.path.join(diretorio_imagens,'Iniciar Jogo UNP.png'))
-inicar_Jogo = pygame.transform.scale(inicar_Jogo, (150, 56.4))
-
+#Imagem do botão "Iniciar Jogo"
+iniciar_Jogo = pygame.image.load(os.path.join(diretorio_imagens,'Iniciar Jogo UNP.png'))
+iniciar_Jogo = pygame.transform.scale(iniciar_Jogo, (150, 56.4))
+iniciar_Jogo_rect = iniciar_Jogo.get_rect()
+iniciar_Jogo_rect.center = (483,372)
+#Background da tela 
 imagem_de_fundo = pygame.image.load(os.path.join(diretorio_imagens, 'Background.png'))
 imagem_de_fundo = pygame.transform.scale(imagem_de_fundo, (largura, altura))
+
+spritesheet_andar_direita = pygame.image.load(os.path.join(diretorio_imagens, 'PassosDireita.png')).convert_alpha()
+spritesheet_andar_esquerda = pygame.image.load(os.path.join(diretorio_imagens, 'PassosEsquerda.png')).convert_alpha()
+
+personagem = Masculino(spritesheet_andar_direita, spritesheet_andar_esquerda)
+sprites_personagem = pygame.sprite.Group()
+sprites_personagem.add(personagem)
+
 # Defina a fonte para o texto
 fonte_usuario = pygame.font.Font(None, 25)
 fonte = pygame.font.Font(None, 30)
 
+clicked = False
 
 # Variável para armazenar o nome do usuário
 nome_usuario = ""
@@ -49,6 +61,7 @@ while rodando:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             rodando = False
+
         elif evento.type == pygame.KEYDOWN:
             # Verificar se uma tecla foi pressionada enquanto o campo de texto está ativo
             if evento.key == pygame.K_BACKSPACE:
@@ -61,6 +74,10 @@ while rodando:
             else:
                 # Adicionar caracteres digitados ao nome do usuário
                 nome_usuario += evento.unicode
+
+        elif evento.type == pygame.MOUSEBUTTONDOWN:
+            if iniciar_Jogo_rect.collidepoint(evento.pos):
+                clicked = True
 
     # Preencher a tela com a cor de fundo
     tela.blit(imagem_de_fundo, (0,0))
@@ -80,10 +97,20 @@ while rodando:
     tela.blit(Usuario_Login, (405, 175))
 
     # Desenhar o botão de iniciar o jogo
-    tela.blit(inicar_Jogo, (405, 372))
+    tela.blit(iniciar_Jogo, iniciar_Jogo_rect,)
+
+    sprites_personagem.draw(tela, (500, 460))
+
+
 
     # Atualizar a tela
     pygame.display.flip()
+
+    if clicked:
+        subprocess.Popen(["python", "Jogo-Plataforma-2D\main.py"])
+        print('Clic')
+        pygame.quit()
+        clicked = False
 
 # Finalizar o Pygame
 pygame.quit()
